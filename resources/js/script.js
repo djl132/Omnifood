27aa36617a6fa0621bcb5bcf21807f2f01df2406
djl132 <djl132@case.edu>
2017-02-06 11:27:20 -0800

    Scrolled Elements
    Imported jQuery.
    
    1. Conditionally displayed sticky-nav using Waypoints.js jQuery library.
    2. Used smooth scroll code snippet.
    
    Tips:
    1. clearly label the html-style-attributes that are used by JS(or any
    code) by js-prefixing them.
    (did not do with ids though, but there is no css tied to that)

---
+++ resources/js/script.js
@@ -0,0 +1,43 @@
+//ADD SCROLLING FUNTIONALITIES
+$(document).ready(function(){
+  
+  //WAYPOINTS FOR DISPLAYING STICKNAV CONDITIONALLY
+  $('.js--section-features').waypoint(function(direction){
+    
+    if(direction=="down")
+      $('nav').addClass('sticky');
+    else
+      $('nav').removeClass('sticky');},
+                                      
+      {offset: '60px'
+    });
+  
+  
+//SCROLL FOR BUTTONS
+  $('.js--scroll-to-plans').click(function(){
+    $('html,body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+  
+  $('.js--scroll-to-start').click(function(){
+    $('html,body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+  
+  
+  //SCROLL FOR LINKS - CODE SNIPPET. HREF a --> id
+  $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+  
+  
+});
