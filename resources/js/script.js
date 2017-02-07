fab5c67cc011aed86bd8c830f39a5bac79e3c4b8
djl132 <djl132@case.edu>
2017-02-06 16:03:17 -0800

    Making Navigation Bar Responsive
    Logic for Animation of Navigation Bar:
    
    1. listen on a parent div and then add classes to style depending on
    whether or not the icon is naviconround(nav is closed) or
    naviconclosed(nav is opened).
    2. slideToggle nav - animates a fade-slide down div taking in duration
    of animation.

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,6 +59,21 @@ $(document).ready(function(){
   
     }, {offset: '50%'
   });
+  
+    
+    /* Mobile navigation */
+    $('.js-nav-icon').click(function() {
+        var nav = $('.js-main-nav');
+        var icon = $('.js-nav-icon i');      
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+        nav.slideToggle(200);
+    });
     
   
 });
